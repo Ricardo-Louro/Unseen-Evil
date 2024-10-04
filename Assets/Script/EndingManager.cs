bdb3b112883f426296b26d149ae3d36cd84cdc1e
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-10-04 13:09:53 +0100

    Allow player to use the cursor to click the button at the EndingScene

--- Assets/Script/EndingManager.cs
+++ Assets/Script/EndingManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class EndingManager : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene(0);
